0afc729496044c5dde60b8c3f700f3c88fb6e961
Giorgi Rostomashvili <giorgi.rostomashvili@inthepocket.com>
2020-05-04 14:40:09 +0200

    updated work experience

--- resume/OpenSource.js
+++ resume/OpenSource.js
@@ -5,7 +5,11 @@ import Separator from './Separator';
 import { BASE_PADDING } from './theme';
 
 const styles = StyleSheet.create({
-  root: { paddingTop: BASE_PADDING, paddingBottom: BASE_PADDING * 2 },
+  root: {
+    paddingTop: 0,
+    paddingBottom: BASE_PADDING * 2,
+    marginTop: -BASE_PADDING * 2
+  },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -19,8 +23,7 @@ const styles = StyleSheet.create({
 });
 
 /* eslint-disable max-len */
-const PR_URL =
-  'https://github.com/facebook/jest/pulls?utf8=%E2%9C%93&q=is%3Apr+author%3Agrosto+';
+const PR_URL = 'https://github.com/pulls?q=+is%3Apr+author%3Agrosto+';
 const ISSUES_URL =
   'https://github.com/facebook/jest/issues?utf8=%E2%9C%93&q=is%3Aissue+grosto';
 /* eslint-enable max-len */
@@ -33,8 +36,8 @@ function OpenSource() {
         <Separator style={styles.separator} />
       </View>
       <Text>
-        Contributor to Jest testing framework since December 2018.{' '}
-        <Link src={PR_URL}>Authored PRs</Link>, and{' '}
+        Contributed to several projects such as Jest testing framework, Deno
+        Javascript Runtime. <Link src={PR_URL}>Authored PRs</Link>, and{' '}
         <Link src={ISSUES_URL}>issues I have helped with</Link>.
       </Text>
     </View>
